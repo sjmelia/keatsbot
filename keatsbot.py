35b4f07af0bead8fe8b3c0c6d43c40d290d0a472
Steve Melia <steve.j.melia@gmail.com>
2015-11-01 09:25:46 +0000

    Up and twittering

---
+++ keatsbot.py
@@ -0,0 +1,25 @@
+import markov
+import random
+from twitter import *
+
+def main():
+    settings = [line.rstrip('\n') for line in file('settings.txt')]
+    source = file('all.txt')
+    generator = markov.Markov(source)
+    length = random.randint(20,140)
+    output = generator.generate_markov_text(length)
+    output = output.lower()
+    print output
+
+    # and now to sing:
+    # wasn't sure which key was which - 
+    # https://gist.github.com/smartboyathome/2599146
+    token = settings[0]    
+    token_key = settings[1] 
+    con_secret = settings[2]
+    con_secret_key = settings[3]
+    twitter = Twitter(auth=OAuth(token, token_key, con_secret_key, con_secret))
+    twitter.statuses.update(status=output)
+
+if __name__ == "__main__":
+    main()
